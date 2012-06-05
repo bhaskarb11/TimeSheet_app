0bcf14db593076a824134fc1409d1299a0b4ac4e
bhaskarb11 <borab0023@gmail.com>
2012-06-05 20:38:36 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeSheetApp::Application.config.secret_token = '552d74eae6122d1032d950e2b26131bd11108c96f969cccc4fdbd91ebe2a710937b9ef29810ae6317b7e73ae7b1c507c2ab3e9b12113f4e7749a2c397ca57eb8'
